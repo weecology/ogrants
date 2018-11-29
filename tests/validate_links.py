7fe9e19b3fbdcf75d9a86eedcca45a8ee8d8ef0d
Shawn Taylor <shawntaylor@weecology.org>
2018-11-29 15:13:15 -0500

    add link validation

---
+++ tests/validate_links.py
@@ -0,0 +1,39 @@
+import yaml
+import glob
+import urllib.request
+
+def link_resolves(url):
+    try:
+        request = urllib.request.urlopen(url)
+        request.close()
+        return True
+    except:
+        return False
+
+def load_grant_file(grant_filename):
+    with open(grant_filename, 'r') as f:
+        lines = f.readlines()
+        # drop the first and last line. pyyaml doesn't like them
+        lines = lines[1:-1]
+        grant_info = yaml.load(''.join(lines))
+    return grant_info
+
+grants = [load_grant_file(f) for f in glob.glob('_grants/*md')]
+
+broken_link_count = 0
+
+for grant in grants:
+    try:
+        grant_year = str(grant['year'])
+    except:
+        grant_year = ''
+        
+    if not link_resolves(grant['link']):
+        print('Grant: ' + str(grant_year) + ' / ' + grant['author'])
+        print('\---------Link Broken---------')
+        print('')
+        broken_link_count+=1
+    else:
+        pass
+
+assert broken_link_count == 0, str(broken_link_count) + ' broken links'
