0cfdee1aab62b857832eb0ce445d0ad30f57ef82
Daniel Himmelstein <daniel.himmelstein@gmail.com>
2019-10-02 15:24:29 -0400

    Update tests and print broken links

--- tests/validate_links.py
+++ tests/validate_links.py
@@ -2,6 +2,7 @@ import yaml
 import glob
 import urllib.request
 
+
 def link_resolves(url):
     try:
         request = urllib.request.urlopen(url)
@@ -10,13 +11,15 @@ def link_resolves(url):
     except:
         return False
 
+
 def load_grant_file(grant_filename):
     with open(grant_filename, 'r') as f:
         lines = f.readlines()
         # drop the first and last line. pyyaml doesn't like them
         lines = lines[1:-1]
-        grant_info = yaml.load(''.join(lines))
+        grant_info = yaml.safe_load(''.join(lines))
     return grant_info
+
 
 grants = [load_grant_file(f) for f in glob.glob('_grants/*md')]
 
@@ -27,18 +30,17 @@ for grant in grants:
         grant_year = str(grant['year'])
     except:
         grant_year = ''
-    
+
     links = grant['link']
     if isinstance(links, str):
         links = [links]
 
     for i, link in enumerate(links):
         if not link_resolves(link):
-            print('Grant: ' + str(grant_year) + ' / ' + grant['author'] + ' / ' + 'Link: ' + str(i + 1))
-            print('\---------Link Broken---------')
-            print('')
-            broken_link_count+=1
+            print(f"Grant: {grant_year} / {grant['author']} / Link: {i + 1}\n{link}")
+            print('---------Link Broken---------\n')
+            broken_link_count += 1
         else:
             pass
 
-assert broken_link_count == 0, str(broken_link_count) + ' broken links'
+assert broken_link_count == 0, f'{broken_link_count} broken links'
