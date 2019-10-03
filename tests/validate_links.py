6ed4046d83cfe38cefb7c723d215a1eda01002da
Daniel Himmelstein <daniel.himmelstein@gmail.com>
2019-10-03 17:13:26 -0400

    empty skip link validation and test refactor

--- tests/validate_links.py
+++ tests/validate_links.py
@@ -1,6 +1,7 @@
-import yaml
-import glob
+import pathlib
 import urllib.request
+
+import yaml
 
 
 def link_resolves(url):
@@ -11,35 +12,36 @@ def link_resolves(url):
         return False
 
 
-def load_grant_file(grant_filename):
-    with open(grant_filename, 'r') as f:
-        lines = f.readlines()
-        # drop the first and last line. pyyaml doesn't like them
-        lines = lines[1:-1]
-        grant_info = yaml.safe_load(''.join(lines))
+def load_grant_file(path):
+    lines = path.read_text().splitlines()
+    # drop the first and last line. pyyaml doesn't like them
+    lines = lines[1:-1]
+    grant_info = yaml.safe_load(''.join(lines))
     return grant_info
 
 
-grants = [load_grant_file(f) for f in glob.glob('_grants/*md')]
+skip_links = set(
+    pathlib.Path(__file__)
+    .parent.joinpath('skip-link-validation.txt')
+    .read_text().splitlines()
+)
 
+grant_paths = sorted(pathlib.Path('_grants').glob('*.md'))
+print(f"{len(grant_paths):,} grant paths detected")
 broken_link_count = 0
-
-for grant in grants:
-    try:
-        grant_year = str(grant['year'])
-    except:
-        grant_year = ''
-
+for path in grant_paths:
+    grant = load_grant_file(path)
+    grant_year = grant.get('year')
     links = grant['link']
     if isinstance(links, str):
         links = [links]
-
+    assert isinstance(links, list)
     for i, link in enumerate(links):
+        if link in skip_links:
+            continue
         if not link_resolves(link):
-            print(f"Grant: {grant_year} / {grant['author']} / Link: {i + 1}\n{link}")
-            print('---------Link Broken---------\n')
+            print('---------Link Broken---------')
+            print(f"Grant: {grant_year} / {grant['author']} / Link: {i + 1}\n{link}\n")
             broken_link_count += 1
-        else:
-            pass
 
 assert broken_link_count == 0, f'{broken_link_count} broken links'
